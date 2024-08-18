d2c3a7f85aa1c6c73ebe6b497571441c6482988b
SriramSololearner <sriram.gsr16@gmail.com>
2024-08-18 18:10:10 +0530

    updated changes in server

--- api/app.js
+++ api/app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(photo));
 
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
 
